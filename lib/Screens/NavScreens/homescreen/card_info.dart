b7aa32d038ebf6bb83ade91e592f613e19734ee8
Elisha <socialcirclescc@gmail.com>
2022-12-31 00:38:12 -0500

    Removed old card(card_data.dart). CardInfo uses Streambuilder. Working on like button

--- lib/Screens/NavScreens/homescreen/card_info.dart
+++ lib/Screens/NavScreens/homescreen/card_info.dart
@@ -77,119 +77,189 @@ class _CardInfoState extends State<CardInfo> {
                 ],
               );
             }
-            return Card(
-              clipBehavior: Clip.antiAlias,
-              child: ConstrainedBox(
-                constraints:
-                    BoxConstraints(minHeight: displayHeight(context) * 0.1),
-                child: Container(
-                  //height: displayHeight(context) * 0.22,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: displayHeight(context) * 0.01,
-                      ),
-                      ListTile(
-                        // ignore: prefer_const_constructors
-                        leading: CircleAvatar(
-                          backgroundImage:
-                              const AssetImage('assets/fp_profile.jpg'),
-                        ),
-                        title: const Text("User's Name"),
-                        subtitle: Text(
-                          "User's Status",
-                          style:
-                              TextStyle(color: Colors.black.withOpacity(0.6)),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          'Line length is determined by typographic parameters based on a formal grid and template with several goals in mind: balance and function for fit and readability with a sensitivity to aesthetic style in typography. Typographers adjust line length to aid legibility or copy fit. Text can be flush left and ragged right, flush right and ragged left, or justified where all lines are of equal length. In a ragged right setting, line lengths vary to create a ragged right edge. Sometimes this can be visually satisfying. For justified and ragged right settings typographers can adjust line length to avoid unwanted hyphens, rivers of white space, and orphaned words/characters at the end of lines (e.g., "The", "I", "He", "We").',
-                          style:
-                              TextStyle(color: Colors.black.withOpacity(0.6)),
-                        ),
-                      ),
-                      // SizedBox(
-                      //   height: displayHeight(context) * 0.01,
-                      // ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 96),
-                        child: Row(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: LikeButton(
-                                circleColor: const CircleColor(
-                                    start: Color(0xff00ddff),
-                                    end: Color(0xff0099cc)),
-                                bubblesColor: const BubblesColor(
-                                  dotPrimaryColor: SecondaryColor,
-                                  dotSecondaryColor: SecondaryColor,
-                                ),
-                                likeBuilder: (bool isLiked) {
-                                  return Icon(
-                                    Icons.thumb_up,
-                                    color: isLiked
-                                        ? PrimaryColor
-                                        : Colors.blueGrey,
-                                    size: 17,
-                                  );
-                                },
-                                likeCount: 0,
-                                countBuilder:
-                                    (count, bool isLiked, String text) {
-                                  var color =
-                                      isLiked ? PrimaryColor : Colors.blueGrey;
-                                  Widget result;
-                                  if (count == 0) {
-                                    result = Text(
-                                      "",
-                                      style: TextStyle(color: color),
-                                    );
-                                  } else {
-                                    result = Text(
-                                      text,
-                                      style: TextStyle(color: color),
-                                    );
-                                  }
-                                  return result;
-                                },
+            return Container(
+              height: displayHeight(context) * 0.8,
+              child: ListView(
+                children: snapshot.data!.docs.map((document) {
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                          minHeight: displayHeight(context) * 0.1),
+                      child: Container(
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: displayHeight(context) * 0.01,
+                            ),
+                            ListTile(
+                              // ignore: prefer_const_constructors
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    const AssetImage('assets/fp_profile.jpg'),
+                              ),
+                              title: Text(document['Name']),
+                              subtitle: Text(
+                                document['Status'],
+                                style: TextStyle(
+                                    color: Colors.black.withOpacity(0.6)),
                               ),
                             ),
                             Padding(
-                              padding: const EdgeInsets.only(left: 90),
-                              child: RichText(
-                                // ignore: prefer_const_constructors
-                                text: TextSpan(
-                                  // ignore: prefer_const_literals_to_create_immutables
-                                  children: [
-                                    WidgetSpan(
-                                      child: Icon(
-                                        Icons.comment_sharp,
-                                        size: 20,
-                                      ),
-                                    ),
-                                    TextSpan(
-                                      text: "0",
-                                      style: TextStyle(
-                                        color: BlackColor,
-                                        fontSize: 12,
-                                      ),
-                                    ),
-                                  ],
+                              padding: const EdgeInsets.all(16.0),
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  document['Post Text'],
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.6)),
                                 ),
                               ),
                             ),
+                            // SizedBox(
+                            //   height: displayHeight(context) * 0.01,
+                            // ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 96),
+                              child: Row(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.all(12.0),
+                                    child: LikeButton(
+                                      onTap: onLikeButtonTapped,
+                                      circleColor: const CircleColor(
+                                          start: Color(0xff00ddff),
+                                          end: Color(0xff0099cc)),
+                                      bubblesColor: const BubblesColor(
+                                        dotPrimaryColor: SecondaryColor,
+                                        dotSecondaryColor: SecondaryColor,
+                                      ),
+                                      likeBuilder: (bool isLiked) {
+                                        return Icon(
+                                          Icons.thumb_up,
+                                          color: isLiked
+                                              ? PrimaryColor
+                                              : Colors.blueGrey,
+                                          size: 17,
+                                        );
+                                      },
+                                      likeCount: document['Likes'],
+                                      countBuilder:
+                                          (count, bool isLiked, String text) {
+                                        var color = isLiked
+                                            ? PrimaryColor
+                                            : Colors.blueGrey;
+                                        Widget result;
+                                        if (count == 0) {
+                                          result = Text(
+                                            "",
+                                            style: TextStyle(color: color),
+                                          );
+                                        } else {
+                                          result = Text(
+                                            text,
+                                            style: TextStyle(color: color),
+                                          );
+                                        }
+                                        return result;
+                                      },
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 90),
+                                    child: RichText(
+                                      // ignore: prefer_const_constructors
+                                      text: TextSpan(
+                                        // ignore: prefer_const_literals_to_create_immutables
+                                        children: [
+                                          WidgetSpan(
+                                            child: Icon(
+                                              Icons.comment_sharp,
+                                              size: 20,
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text: "0",
+                                            style: TextStyle(
+                                              color: BlackColor,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
                           ],
                         ),
-                      )
-                    ],
-                  ),
-                ),
+                      ),
+                    ),
+                  );
+                }).toList(),
               ),
             );
           }),
     );
   }
+
+  Future<bool> onLikeButtonTapped(bool isLiked) async{
+    //if isLiked = true, then add 1 to the likes
+    //if isLiked = false, then substract 1 from the likes
+
+    var collection = FirebaseFirestore.instance
+      .collection("Churches")
+      .doc(churchID)
+     .collection("Posts")
+      .doc()//get the document ID
+      .update({'key' : 'value'}) // <-- Updated data
+      .then((_) => print('Success'))
+      .catchError((error) => print('Failed: $error'));
+
+    return !isLiked;
+  }
 }
+
+
+
+
+
+
+  // //Get Posts
+  // Future getPosts() async {
+  //   int counter = 0;
+  //   List<Map<String, dynamic>> tempPostData = [];
+  //   var random;
+
+  //   FirebaseAuth auth = FirebaseAuth.instance;
+  //   final User? user = auth.currentUser;
+  //   final uid = user?.uid;
+
+  //   var postData = await FirebaseFirestore.instance
+  //       .collection('Churches')
+  //       .doc(churchID)
+  //       .collection("Posts")
+  //       .get()
+  //       .then((QuerySnapshot snapShot) {
+  //     for (var element in snapShot.docs) {
+  //       if (element.exists) {
+  //         print("Collection Exists!!!!");
+  //         print(element['Name']);
+  //         random = element['Name'];
+  //         // tempPostData.add({"Name" + counter.toString(): element['Name']});
+  //         // tempPostData.add({"Post": element['Post Text']});
+  //         // tempPostData.add({"Status": element['Status']});
+  //         // tempPostData.add({"Likes": element['Likes']});
+  //         print("Where the error is.");
+  //       }
+  //     }
+  //   });
+
+  //   if (this.mounted) {
+  //     setState(() {
+  //       postData = tempPostData;
+  //       randomData = random;
+  //     });
+  //   }
+  // }
--- lib/Screens/NavScreens/homescreen/home_screen.dart
+++ lib/Screens/NavScreens/homescreen/home_screen.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:community/screens/navscreens/homescreen/card_data.dart';
 import 'package:community/screens/navscreens/homescreen/card_info.dart';
 import 'package:flutter/material.dart';
 
@@ -11,13 +10,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List _posts = [
-    'Post 1',
-    'Post 2',
-    'Post 3',
-    'Post 4',
-  ];
-
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -26,31 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
             content: Text('The System Back Button is Deactivated')));
         return false;
       },
-      child: FutureBuilder(
-          future: Future.wait([
-            //getUserInfo(),
-          ]),
-          builder:
-              (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-            if (!snapshot.hasData) {
-              return const CircularProgressIndicator();
-            }
-
-            return Scaffold(
-              body: Column(
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                        itemCount: _posts.length,
-                        itemBuilder: (context, index) {
-                          return CardInfo();
-                        }),
-                  ),
-                ],
-              ),
-            );
-          }),
+      child: CardInfo(),
     );
   }
 }
